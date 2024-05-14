648773d045e42ef2a6e11d3cfec9ddc0ad6f990f
FredMbugua <fredmbugua004@gmail.com>
2024-05-14 02:40:26 -0700

    add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from add_0 import add
+#initialize variables
+a = 1
+b = 2
+#initialize variable to funtion
+addition = add(1, 2)
+#print addition
+print('{} + {} = {}'.format(a, b, addition))
