9bd2552663aac646b8d2172a4ac8f4ed0f2da42f
FredMbugua <fredmbugua004@gmail.com>
2024-05-15 11:46:11 -0700

    Calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,16 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 from calculator_1 import add, sub, div, mul
-# initialize variables
-a = 10
-b = 5
-# initialize varibles to function call
-addition = add(a, b)
-subtract = sub(a, b)
-divide = div(a, b)
-multiply = mul(a, b)
-# print output
-print('{} + {} = {}'.format(a, b, addition))
-print('{} - {} = {}'.format(a, b, subtract))
-print('{} * {} = {}'.format(a, b, multiply))
-print('{} / {} = {}'.format(a, b, divide))
+if __name__ == "__main__":
+    # initialize variables
+    a = 10
+    b = 5
+    # initialize varibles to function call
+    addition = add(a, b)
+    subtract = sub(a, b)
+    divide = div(a, b)
+    multiply = mul(a, b)
+    # print output
+    print('{} + {} = {}'.format(a, b, addition))
+    print('{} - {} = {}'.format(a, b, subtract))
+    print('{} * {} = {}'.format(a, b, multiply))
+    print('{} / {} = {}'.format(a, b, divide))
