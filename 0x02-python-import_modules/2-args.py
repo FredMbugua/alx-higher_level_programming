4b3a84e4541c404196e045140e15b0e65497dc10
FredMbugua <fredmbugua004@gmail.com>
2024-05-15 11:33:00 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     num = len(argv)
     argc = len(argv) - 1
     if argc == 0:
-        print('{} arguments'.format(argc))
+        print('{} arguments.'.format(argc))
     elif argc == 1:
-        print("{} argument:".format(argc))
-        print("{}: {}".format(argc, argv[1]))
+        print('{} argument:'.format(argc))
+        print('{}: {}'.format(argc, argv[1]))
     elif argc > 1:
         print('{} arguments'.format(argc))
         for i in range(1, num):
