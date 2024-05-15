7ce29e639d6a802baf57136d0bceaf5593f938bb
FredMbugua <fredmbugua004@gmail.com>
2024-05-15 12:11:55 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-# initialize variables
+
     a = 1
     b = 2
-# initialize variable to funtion
-    addition = add(1, 2)
-# print addition
-    print('{} + {} = {}'.format(a, b, addition))
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
