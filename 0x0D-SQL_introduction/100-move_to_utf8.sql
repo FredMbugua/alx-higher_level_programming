d59bc0650b716f69915feca82c8ed835a4c2908d
FredMbugua <fredmbugua004@gmail.com>
2024-05-22 15:18:30 -0700

    Advanced

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- Change the default character set and collation of the database
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Change the character set and collation of the table
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Change the character set and collation of the 'name' field in the table
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- top city
+SELECT city, AVG(temperature) AS avg_temp
+FROM temperatures
+WHERE month IN ('July', 'August')
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
+
