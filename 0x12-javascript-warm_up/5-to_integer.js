dafbae7a2fc21daa573a7838e6b5d00b14ba320c
FredMbugua <fredmbugua004@gmail.com>
2024-06-19 03:02:16 -0700

    Convert Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 const firstArg = args[0];
 const convertedNumber = parseInt(firstArg, 10);
 if (isNaN(convertedNumber)) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
   console.log(`My number: ${convertedNumber}`);
 }
