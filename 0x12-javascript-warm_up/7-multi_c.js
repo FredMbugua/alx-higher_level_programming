b35c916f2fe441133f46aa804538187983dbbf3f
FredMbugua <fredmbugua004@gmail.com>
2024-06-19 04:10:19 -0700

    Multi C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const x = parseInt(args[0], 10);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let count = 0;
+  while (count < x) {
+    console.log('C is fun');
+    count++;
+  }
+}
